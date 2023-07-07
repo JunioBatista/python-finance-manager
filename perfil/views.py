b63a4bdfa93ce7230c3b55b475bd730e4edcb515
JunioBatista <juniorjob21@gmail.com>
2023-07-07 02:33:44 -0300

    set dinamic values

--- perfil/views.py
+++ perfil/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Conta
+from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
-
+from .utils import calcula_total
 
 
 def home(request):
-    return render(request, 'home.html')
+    contas = Conta.objects.all()
+
+    valor_total = calcula_total(contas, 'valor')
+    return render(request, 'home.html', {'contas': contas, 'valor_total': valor_total})
 
 def gerenciar(request):
-    return render(request, 'gerenciar.html')
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+
+    valor_total = calcula_total(contas, 'valor')
+        
+    return render(request, 'gerenciar.html', {'contas': contas, 'valor_total': valor_total, 'categorias':categorias })
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -34,5 +42,34 @@ def cadastrar_banco(request):
     conta.save()
     messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso!')
 
+
+    return redirect('/perfil/gerenciar/')
+
+
+def deletar_banco(request, id):  
+    conta = Conta.objects.get(id=id)
+    conta.delete()
+
+    messages.add_message(request, constants.SUCCESS, 'Conta deletada com sucesso!')
+    return redirect('/perfil/gerenciar/')
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+    categoria.essencial = not categoria.essencial
+    categoria.save()
 
     return redirect('/perfil/gerenciar/')
