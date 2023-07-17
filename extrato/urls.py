034e5a38c0f9eb1fd3d4f6149b3c663587ccfd15
JunioBatista <juniorjob21@gmail.com>
2023-07-17 03:24:07 -0300

    exportar pdf incompleto

--- extrato/urls.py
+++ extrato/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('novo_valor/', views.novo_valor, name="novo_valor"),
-    path('view_extrato/', views.view_extrato, name="noview_extratovo_valor"),
+    path('view_extrato/', views.view_extrato, name="view_extrato"),
+    path('exportar_pdf/', views.exportar_pdf, name="exportar_pdf"),
+
+    
 
 ]
